79a6718042b0fc1b667a373794047143c96e6c65
techchee <ketyung@techchee.com>
2021-07-28 21:10:54 +0800

    Ummm seems the remote repo missing the InfoViewModel ? Why??

---
+++ SwiftUITestStateObservedObject/TestView.swift
@@ -0,0 +1,16 @@
+//
+//  TestView.swift
+//  SwiftUITestStateObservedObject
+//
+//  Created by Chee Ket Yung on 28/07/2021.
+//
+
+import SwiftUI
+
+struct TestView : View {
+    
+    var body: some View{
+        
+        Text("test")
+    }
+}
