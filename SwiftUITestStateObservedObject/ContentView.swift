ceaee4a46055428b0621b701c620d537540b3bfc
techchee <ketyung@techchee.com>
2021-07-28 18:56:14 +0800

    Initial Commit

---
+++ SwiftUITestStateObservedObject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITestStateObservedObject
+//
+//  Created by Chee Ket Yung on 28/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
